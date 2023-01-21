f702d659550ed3ab8faae513d37c38d452ad68f5
bimarshak7 <khanalbimarsha7@gmail.com>
2023-01-21 18:05:47 +0545

    Remove junk codes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,14 @@ import { store } from "./redux/store"
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 
 import "./App.css"
-import {
-} from "./pages"
+import {} from "./pages"
 import { PrivateRoute } from "./components/common"
 
 function App() {
 	return (
 		<Provider store={store}>
 			<Router>
-				<Routes>
-				</Routes>
+				<Routes></Routes>
 			</Router>
 		</Provider>
 	)
